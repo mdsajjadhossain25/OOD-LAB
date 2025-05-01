5eb1fcd8ecf8b91014952c85a922a4d206ac5c60
mdsajjadhossain25 <md.sajjad.hossain.025@gmail.com>
2025-05-02 02:23:14 +0600

    command pattern implemented

---
+++ command/CommandPatternDemo.java
@@ -0,0 +1,83 @@
+package command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Command interface
+ interface Order {
+    void execute();
+}
+
+// Receiver class
+ class Stock {
+    private String name = "ABC";
+    private int quantity = 10;
+
+    public void buy() {
+        System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] bought");
+    }
+
+    public void sell() {
+        System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] sold");
+    }
+}
+
+// Concrete Command for buying stock
+ class BuyStock implements Order {
+    private Stock abcStock;
+
+    public BuyStock(Stock abcStock) {
+        this.abcStock = abcStock;
+    }
+
+    @Override
+    public void execute() {
+        abcStock.buy();
+    }
+}
+
+// Concrete Command for selling stock
+ class SellStock implements Order {
+    private Stock abcStock;
+
+    public SellStock(Stock abcStock) {
+        this.abcStock = abcStock;
+    }
+
+    @Override
+    public void execute() {
+        abcStock.sell();
+    }
+}
+
+// Invoker class
+ class Broker {
+    private List<Order> orderList = new ArrayList<>();
+
+    public void takeOrder(Order order) {
+        orderList.add(order);
+    }
+
+    public void placeOrders() {
+        for (Order order : orderList) {
+            order.execute();
+        }
+        orderList.clear();
+    }
+}
+
+// Client class
+public class CommandPatternDemo {
+    public static void main(String[] args) {
+        Stock abcStock = new Stock();
+
+        BuyStock buyStockOrder = new BuyStock(abcStock);
+        SellStock sellStockOrder = new SellStock(abcStock);
+
+        Broker broker = new Broker();
+        broker.takeOrder(buyStockOrder);
+        broker.takeOrder(sellStockOrder);
+
+        broker.placeOrders();
+    }
+}
